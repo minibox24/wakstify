439ec9c7b032f8976e229b3cca4395b13f2b778a
minibox24 <minibox724@gmail.com>
2022-08-25 22:57:30 +0900

    기초 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+interface Track {
+  id: string;
+  date: string;
+  start: number;
+  end: number;
+
+  artist: string;
+  title: string;
+  youtube: string;
+}
+
+export default function App() {
+  const [tracks, setTracks] = React.useState<Track[]>([]);
+
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+  const [info, setInfo] = React.useState<string>("");
+
+  React.useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:8000/tracks");
+      const data = await response.json();
+
+      setTracks(data);
+    })();
+  }, []);
+
+  const playTrack = async (track: Track) => {
+    const response = await fetch(`http://localhost:8000/tracks/${track.id}`);
+    const data = await response.json();
+
+    if (videoRef.current) {
+      setInfo(`Playing ${track.title}`);
+      videoRef.current.src = data.url;
+      videoRef.current.currentTime = data.track.start;
+      videoRef.current.play();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{info}</h1>
+      <video ref={videoRef} width="800px" autoPlay controls />
+      <div style={{ display: "flex" }}>
+        {tracks.map((track) => (
+          <div
+            key={track.id}
+            onClick={() => {
+              playTrack(track);
+            }}
+            style={{
+              border: "1px solid black",
+              width: "200px",
+              margin: "10px",
+              padding: "10px",
+              cursor: "pointer",
+            }}
+          >
+            <img
+              alt="cover"
+              src={`https://img.youtube.com/vi/${track.youtube}/maxresdefault.jpg`}
+              style={{
+                width: "200px",
+              }}
+            />
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <span>{track.artist}</span>
+              <span>{track.title}</span>
+              <span>{track.date}</span>
+              <span>
+                {new Date((track.end - track.start) * 1000)
+                  .toISOString()
+                  .substring(14, 19)}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
