62f698bc69ca0a823e45c3c4b39dd4fe558635f6
minibox24 <minibox724@gmail.com>
2022-08-30 00:13:19 +0900

    아이콘

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,19 @@
+import ForwardIcon from "./ForwardIcon";
+import PreviousIcon from "./PreviousIcon";
+
+import PauseIcon from "./PauseIcon";
+import PlayIcon from "./PlayIcon";
+
+import LoopIcon from "./LoopIcon";
+import LoopOneIcon from "./LoopOneIcon";
+import ShuffleIcon from "./ShuffleIcon";
+
+export {
+  ForwardIcon,
+  PreviousIcon,
+  PauseIcon,
+  PlayIcon,
+  LoopIcon,
+  LoopOneIcon,
+  ShuffleIcon,
+};
